1366433bdd52b06f88b4176a7bd412177d96c6f2
Michael Maltsev <4129781+m417z@users.noreply.github.com>
2024-10-19 00:41:19 +0300

    v1.3

--- common/version.h
+++ common/version.h
@@ -4,7 +4,7 @@
 
 // Version
 #define VERSION_MAJOR               1
-#define VERSION_MINOR               2
+#define VERSION_MINOR               3
 #define VERSION_REVISION            0
 #define VERSION_BUILD               0
 
