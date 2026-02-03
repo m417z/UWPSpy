4d9088b64ddf3beded0d3df5750975babd1d1b33
Michael Maltsev <4129781+m417z@users.noreply.github.com>
2026-02-04 01:14:48 +0200

    Add process selection Ctrl+D tip

--- UWPSpyLauncher/resource.h
+++ UWPSpyLauncher/resource.h
@@ -8,7 +8,8 @@
 #define IDC_STATIC_FRAMEWORK            1001
 #define IDC_RADIO_UWP                   1002
 #define IDC_RADIO_WINUI                 1003
-#define IDC_REFRESH_BUTTON              1004
+#define IDC_STATIC_TIP                  1004
+#define IDC_REFRESH_BUTTON              1005
 
 // Next default values for new objects
 // 
@@ -16,7 +17,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        200
 #define _APS_NEXT_COMMAND_VALUE         32775
-#define _APS_NEXT_CONTROL_VALUE         1005
+#define _APS_NEXT_CONTROL_VALUE         1006
 #define _APS_NEXT_SYMED_VALUE           100
 #endif
 #endif
